d3c7a79c067f90426ca7557d796ca3a3ac6bf9c0
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-24 19:09:53 +0530

    Margin issue fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-[12px] sm:text-3xl font-extrabold underline flex justify-center mt-96 text-white">
+      <h1 className="text-md text-center sm:text-3xl font-extrabold underline flex justify-center mt-72 text-white">
         Hello there 👋, this is Dhruv Pankaj Patel's portfolio website.
       </h1>
     </>
