2852077ac456019bc73ff5192fde5b3a338d25f6
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-25 00:56:03 +0530

    animation speed increased

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl md:text-8xl font-extrabold text-center leading-snug flex justify-center mt-64">
+      <h1 className="text-3xl md:text-8xl font-extrabold text-center leading-snug flex justify-center mt-[325px] md:mt-72">
         👋 Hi!, I am Dhruv
       </h1>
     </>
