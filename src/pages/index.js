6925189efbb5f7017cf450ffbe3895ee44ec4da8
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-25 00:38:10 +0530

    minor fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-5xl font-extrabold text-center leading-snug flex justify-center mt-72">
-        Hello there 👋, I am Dhruv and this is my website.
+      <h1 className="text-3xl md:text-8xl font-extrabold text-center leading-snug flex justify-center mt-64">
+        👋 Hi!, I am Dhruv
       </h1>
     </>
   )
