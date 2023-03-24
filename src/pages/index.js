46a57fb25df1d326e992a58c238a8800fa0dc6f6
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-24 21:06:43 +0530

    Added Gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-md text-center sm:text-3xl font-extrabold underline flex justify-center mt-72 text-white">
-        Hello there 👋, this is Dhruv Pankaj Patel's portfolio website.
+      <h1 className="text-5xl font-extrabold text-center leading-snug flex justify-center mt-72">
+        Hello there 👋, I am Dhruv and this is my website.
       </h1>
     </>
   )
