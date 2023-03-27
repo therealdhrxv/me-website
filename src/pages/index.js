8cb6a8ce79b451d77628851579590d7744314ce7
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-28 02:38:18 +0530

    Social links added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl md:text-8xl font-extrabold text-center leading-snug flex justify-center mt-[325px] md:mt-72">
+      <div className="text-[45px] md:text-8xl font-extrabold text-center leading-snug flex justify-center mt-[250px] md:mt-60">
         👋 Hi!, I am Dhruv
-      </h1>
+      </div>
+      <div className="flex mt-11 md:mt-28 justify-evenly">
+        <a href="https://github.com/therealdhruv">
+          <img src={"/socialImages/g.svg"} className="w-14 md:w-20 md:h-20" />
+        </a>
+        <a href="https://linkedin.com/in/therealdhruv">
+          <img src={"/socialImages/i.svg"} className="w-14 md:w-20 md:h-20" />
+        </a>
+        <a href="https://twitter.com/thereal_dhruv">
+          <img src={"/socialImages/t.svg"} className="w-14 md:w-20 md:h-20" />
+        </a>
+        <a href="https://instagram.com/thereal_dhruv">
+          <img src={"/socialImages/insta.svg"} className="w-14 md:w-20 md:h-20" />
+        </a>
+      </div>
     </>
-  )
+  );
 }
