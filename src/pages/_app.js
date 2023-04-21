d1adcd45e1da7df1ba405331ac24861dc42057e7
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-04-21 16:07:05 +0530

    removed head component from _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,38 +1,22 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import { MantineProvider } from "@mantine/core";
 import { ToastContainer } from "react-toastify";
-import { NextSeo } from "next-seo";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 
-
 export default function App(props) {
   const { Component, pageProps } = props;
-
   return (
     <>
-      <Head>
-        <title>Dhruv Pankaj Patel</title>
-        <meta name="author" content="Portfolio Website" />
-        <meta
-          name="keywords"
-          content="Dhruv Pankaj Patel, Dhruv, Dhruv vit, Dhruv pankaj, Dhruv pankaj patel, Dhruv patel, Dhruv pankaj patel vit, Dhruv pankaj patel vit vellore, Dhruv pankaj patel vellore, Dhruv pankaj patel vellore institute of technology, Dhruv pankaj patel vellore institute of technology vellore, Dhruv pankaj patel vellore institute of technology vellore tamil nadu, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014 dhruv pankaj patel, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014 dhruv pankaj patel dhruv pankaj patel, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014 dhruv pankaj patel dhruv pankaj patel dhruv pankaj patel, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014 dhruv pankaj patel dhruv pankaj patel dhruv pankaj patel dhruv pankaj patel, Dhruv pankaj patel vellore institute of technology vellore tamil nadu india 632014 dhruv"
-        />
-      </Head>
       <NextSeo
         title="Dhruv Pankaj Patel"
         description="This is the portfolio website of Dhruv Pankaj Patel. Dhruv is a Information Technology Engineering undergraduate student at Vellore Institute of Technology, Vellore, Tamil Nadu, India."
-        twitter={{
-          handle: "@thereal_dhruv",
-          cardType: "summary_large_image",
-        }}
+        twitter={{ handle: "@thereal_dhruv", cardType: "summary_large_image" }}
       />
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
-        theme={{
-          colorScheme: "light",
-        }}
+        theme={{ colorScheme: "light" }}
       >
         <Component {...pageProps} />
         <ToastContainer />
