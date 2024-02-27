6dc900490d0dc88a2d9f79bc01b15ee1742ee846
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-02-27 17:23:48 +0530

    fix: username update - therealdhrxv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,66 @@
 import { NextSeo } from "next-seo";
 
-const socialMediaLinks = [
-  {
-    href: "https://github.com/therealdhruv",
-    ariaLabel: "Visit Dhruv's GitHub profile",
-    src: "/assets/github.png",
-    alt: "GitHub logo",
-  },
-  {
-    href: "https://linkedin.com/in/therealdhruv",
-    ariaLabel: "Visit Dhruv's LinkedIn profile",
-    src: "/assets/linkedin.png",
-    alt: "LinkedIn logo",
-  },
-  {
-    href: "https://twitter.com/thereal_dhruv",
-    ariaLabel: "Visit Dhruv's Twitter profile",
-    src: "/assets/twitter.png",
-    alt: "Twitter logo",
-  },
-  {
-    href: "https://instagram.com/thereal_dhruv",
-    ariaLabel: "Visit Dhruv's Instagram profile",
-    src: "/assets/instagram.png",
-    alt: "Instagram logo",
-  },
+const socials = [
+	{
+		href: "https://github.com/therealdhrxv",
+		ariaLabel: "Visit Dhruv's GitHub profile",
+		src: "/assets/github.png",
+		alt: "GitHub logo",
+	},
+	{
+		href: "https://linkedin.com/in/therealdhrxv",
+		ariaLabel: "Visit Dhruv's LinkedIn profile",
+		src: "/assets/linkedin.png",
+		alt: "LinkedIn logo",
+	},
+	{
+		href: "https://twitter.com/therealdhrxv",
+		ariaLabel: "Visit Dhruv's Twitter profile",
+		src: "/assets/twitter.png",
+		alt: "Twitter logo",
+	},
+	{
+		href: "https://instagram.com/therealdhrxv",
+		ariaLabel: "Visit Dhruv's Instagram profile",
+		src: "/assets/instagram.png",
+		alt: "Instagram logo",
+	},
 ];
 
 export default function Home() {
-  return (
-    <>
-      <NextSeo
-        title="Dhruv Pankaj Patel"
-        description="This is the portfolio website of Dhruv Pankaj Patel. Dhruv is a Information Technology Engineering undergraduate student at Vellore Institute of Technology, Vellore, Tamil Nadu, India."
-        canonical="https://therealdhruv.tech/"
-        openGraph={{
-          url: "https://therealdhruv.tech/",
-          title: "Dhruv Pankaj Patel",
-          description:
-            "This is the portfolio website of Dhruv Pankaj Patel. Dhruv is a Information Technology Engineering undergraduate student at Vellore Institute of Technology, Vellore, Tamil Nadu, India.",
-          images: [
-            {
-              url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F92241443%3Fv%3D4&tbnid=Gy6fb0GoiOlMAM&vet=12ahUKEwjJkMjDq5L-AhXFF2IAHaRXCowQMygAegQIARBP..i&imgrefurl=https%3A%2F%2Fgithub.com%2Ftherealdhruv&docid=qFGwyBru2MfbsM&w=400&h=400&q=dhruv%20pankaj%20patel&ved=2ahUKEwjJkMjDq5L-AhXFF2IAHaRXCowQMygAegQIARBP",
-              width: 1200,
-              height: 630,
-              alt: "Dhruv Pankaj Patel",
-            },
-          ],
-          site_name: "Dhruv Pankaj Patel",
-        }}
-        twitter={{
-          handle: "@thereal_dhruv",
-          site: "@thereal_dhruv",
-          cardType: "summary_large_image",
-        }}
-      />
+	return (
+		<>
+			<NextSeo
+				title="Dhruv Pankaj Patel"
+				description="This is the portfolio website of Dhruv Pankaj Patel. Dhruv is a Information Technology Engineering undergraduate student at Vellore Institute of Technology, Vellore, Tamil Nadu, India."
+				canonical="https://therealdhruv.tech/"
+				openGraph={{
+					url: "https://therealdhruv.tech/",
+					title: "Dhruv Pankaj Patel",
+					description:
+						"This is the portfolio website of Dhruv Pankaj Patel. Dhruv is a Information Technology Engineering undergraduate student at Vellore Institute of Technology, Vellore, Tamil Nadu, India.",
+					images: [
+						{
+							url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F92241443%3Fv%3D4&tbnid=Gy6fb0GoiOlMAM&vet=12ahUKEwjJkMjDq5L-AhXFF2IAHaRXCowQMygAegQIARBP..i&imgrefurl=https%3A%2F%2Fgithub.com%2Ftherealdhruv&docid=qFGwyBru2MfbsM&w=400&h=400&q=dhruv%20pankaj%20patel&ved=2ahUKEwjJkMjDq5L-AhXFF2IAHaRXCowQMygAegQIARBP",
+							width: 1200,
+							height: 630,
+							alt: "Dhruv Pankaj Patel",
+						},
+					],
+					site_name: "Dhruv Pankaj Patel",
+				}}
+				twitter={{
+					handle: "@therealdhrxv",
+					site: "@therealdhrxv",
+					cardType: "summary_large_image",
+				}}
+			/>
 
-      <header>
-        <div className="text-[45px] md:text-7xl font-extrabold text-center leading-snug flex justify-center mt-10 md:mt-16">
-          👋
-          <span
-            className="
+			<header>
+				<div className="text-[45px] md:text-7xl font-extrabold text-center leading-snug flex justify-center mt-10 md:mt-16">
+					👋
+					<span
+						className="
             bg-[length:300%_300%] 
             bg-no-repeat animate-bgMove bg-clip-text text-transparent md:ml-7 ml-3
             bg-gradient-to-r 
@@ -69,39 +69,39 @@ export default function Home() {
             via-[#ff8fc5] 
             to-[#5bb8ff]
           "
-          >
-            Hi!, I am Dhruv
-          </span>
-        </div>
-      </header>
+					>
+						Hi!, I am Dhruv
+					</span>
+				</div>
+			</header>
 
-      <div className="font-mono md:mt-[7%] md:w-[50%] md:ml-[25%] text-xl md:text-left text-left ml-4 mr-4 mt-10 leading-9">
-        I am a student of the 2021 batch of Information Technology at the
-        Vellore Institute of Technology, Vellore. I primarily write programs
-        using JavaScript, TypeScript and Java though I have experimented with
-        other languages such as Python, Assembly (x86_64), R and C/C++. <br />{" "}
-        <br />
-        My hobbies include playing chess, and binging too many videos on
-        YouTube. I am currently learning various frontend technologies and it's
-        integration with the backend.
-      </div>
+			<div className="font-mono md:mt-[7%] md:w-[50%] md:ml-[25%] text-xl md:text-left text-left ml-4 mr-4 mt-10 leading-9">
+				I am a student of the 2021 batch of Information Technology
+				at the Vellore Institute of Technology, Vellore. I
+				primarily write programs using JavaScript, TypeScript and
+				Java though I have experimented with other languages such
+				as Python, Assembly (x86_64), R and C/C++. <br /> <br />
+				My hobbies include playing chess, and binging too many
+				videos on YouTube. I am currently learning various frontend
+				technologies and it's integration with the backend.
+			</div>
 
-      <div className="md:mt-[8%] md:w-[60%] md:ml-[19%] mb-12 flex justify-evenly mt-14 md:space-x-20">
-        {socialMediaLinks.map((link, index) => (
-          <a
-            href={link.href}
-            aria-label={link.ariaLabel}
-            target="_blank"
-            key={index}
-          >
-            <img
-              src={link.src}
-              className="w-14 md:w-15 md:h-15"
-              alt={link.alt}
-            />
-          </a>
-        ))}
-      </div>
-    </>
-  );
+			<div className="md:mt-[8%] md:w-[60%] md:ml-[19%] mb-12 flex justify-evenly mt-14 md:space-x-20">
+				{socials.map((link, index) => (
+					<a
+						href={link.href}
+						aria-label={link.ariaLabel}
+						target="_blank"
+						key={index}
+					>
+						<img
+							src={link.src}
+							className="w-14 md:w-15 md:h-15"
+							alt={link.alt}
+						/>
+					</a>
+				))}
+			</div>
+		</>
+	);
 }
